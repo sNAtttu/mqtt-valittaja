caf9c0b48e1ac3abe3acc517e98b9ac25ad515a0
sNAttu <snattu91@gmail.com>
2020-09-18 23:44:40 +0300

    PINGREQ implemented for keepalive

--- source/services/packetService.ts
+++ source/services/packetService.ts
@@ -1,4 +1,9 @@
-import { IPacket, IConnackPacket, generate } from "mqtt-packet";
+import {
+  IPacket,
+  IConnackPacket,
+  generate,
+  IPingrespPacket,
+} from "mqtt-packet";
 import * as winston from "winston";
 import WebSocket from "ws";
 import Logger from "../logger";
@@ -13,16 +18,30 @@ export default class PacketService {
   receivePacket(mqttPacket: IPacket, websocketInstance: WebSocket) {
     switch (mqttPacket.cmd) {
       case ClientToServerTypes.CONNECT:
-        const packetContents: IConnackPacket = {
-          cmd: "connack",
-          returnCode: 0,
-          sessionPresent: false,
-        };
-        const responsePacket = generate(packetContents);
-        websocketInstance.send(responsePacket);
+        this.logger.info("Client connected");
+        const connackPacket = generate(this.buildConnackPacket());
+        websocketInstance.send(connackPacket);
         break;
+      case ClientToServerTypes.PINGREQ:
+        this.logger.info("Client requested for ping");
+        const pingRespPacket = generate(this.buildPingRespPacket());
+        websocketInstance.send(pingRespPacket);
       default:
         break;
     }
+  }
+
+  private buildPingRespPacket(): IPingrespPacket {
+    return {
+      cmd: "pingresp",
+    };
+  }
+
+  private buildConnackPacket(): IConnackPacket {
+    return {
+      cmd: "connack",
+      returnCode: 0,
+      sessionPresent: false,
+    };
   }
 }
